89e58e5bfb41a8f8026765eabefa11ad49145ddb
Menashe <moses2787@gmail.com>
2019-02-06 00:23:05 -0700

    all buttons should be fully functional, including reset

--- main.js
+++ main.js
@@ -7,18 +7,16 @@ var maxNumber = document.querySelector('.max-value');
 var maxRange = document.querySelector('#max-range-input');
 var minNumber = document.querySelector('.min-value');
 var minRange = document.querySelector('#min-range-input');
+var name1Result = document.querySelector('.name1-result');
+var name2Result = document.querySelector('.name2-result');
 var player1Input = document.querySelector('#player1');
 var player2Input = document.querySelector('#player2');
+var randomNumber = null;
 var resetButton = document.querySelector('.reset-game-button');
 var result1 = document.querySelector('.result1');
 var result2 = document.querySelector('.result2');
 var submitButton = document.querySelector('.submit-guess-button');
 var updateButton = document.querySelector('.update-button');
-var randomNumber = null;
-
-// updateButton.disabled = true;
-// submitButton.disabled = true;
-
 
 updateButton.addEventListener('click', function(e) {
   e.preventDefault();
@@ -28,22 +26,16 @@ updateButton.addEventListener('click', function(e) {
   console.log(randomNumber);
 })
 
-
 submitButton.addEventListener('click', function(e) {
   e.preventDefault();
   p1GuessEval();
   p2GuessEval();
+  changeNames();
 })
 
 resetButton.addEventListener('click', function(e) {
   e.preventDefault();
-  guess1Input.value = '';
-  guess2Input.value = '';
-  player1Input.value = '';
-  player2Input.value = '';
-  minRange.value = '';
-  maxRange.value = '';
-  randomNumber = null;
+  resetFields();
 })
 
 clearButton.addEventListener('click', function(e) {
@@ -56,6 +48,29 @@ function random() {
   var random = Math.floor(Math.random() * (parseInt(maxRange.value) - parseInt(minRange.value) + 1)) + parseInt(minRange.value);
   return random;
 };
+
+function resetFields () {
+  guess1Input.value = '';
+  guess2Input.value = '';
+  player1Input.value = '';
+  player2Input.value = '';
+  minRange.value = '';
+  maxRange.value = '';
+  randomNumber = null;
+  minNumber.innerText = '?';
+  maxNumber.innerText = '?';
+  displayGuess1.innerText = '?';
+  displayGuess2.innerText = '?';
+  name1Result.innerText = 'Challenger 1';
+  name2Result.innerText = 'Challenger 2';
+  result1.innerText = 'Your result';
+  result2.innerText = 'Your result';
+}
+
+function changeNames() {
+  name1Result.innerText = player1Input.value;
+  name2Result.innerText = player2Input.value;
+}
 
 function p1GuessEval() {
   displayGuess1.innerText = guess1Input.value;
